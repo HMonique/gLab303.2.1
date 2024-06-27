4721390e072e9b7e7ca64d287e2bb5ba9919e0a5
HMonique <161358207+HMonique@users.noreply.github.com>
2024-06-27 09:55:26 -0400

    gLab303.2.1

--- src/BitWiseOperatorsDemo.java
+++ src/BitWiseOperatorsDemo.java
@@ -1,7 +1,7 @@
 public class BitWiseOperatorsDemo {
     public static void main(String[] args)
     {
-        int x = 50;
+        int x = 58;
         int y = 13;
         System.out.println("x & y : " + (x & y));
         System.out.println("x | y : " + (x | y));
