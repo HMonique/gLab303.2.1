f5e54ebc69fd40b720aea58e390eef4f8686ff60
HMonique <161358207+HMonique@users.noreply.github.com>
2024-06-27 09:28:47 -0400

    gLab303.2.1

--- src/AssignmentOperatorDemo.java
+++ src/AssignmentOperatorDemo.java
@@ -3,15 +3,15 @@
 public class AssignmentOperatorDemo {
     public static void main(String[] args) {
 
-   int j, k;
-   j = 10;
-   j = 5;
-   k = j;
-   System.out.println("j is : " + j);
-   System.out.println("k is : " + k);
+       int j, k;
+       j = 10;
+       j = 5;
+       k = j;
+       System.out.println("j is : " + j);
+       System.out.println("k is : " + k);
 
-   k = j = 10;
-   System.out.println("j is : " + j);
-   System.out.println("k is : " + k);
+       k = j = 10;
+       System.out.println("j is : " + j);
+       System.out.println("k is : " + k);
     }
 }
