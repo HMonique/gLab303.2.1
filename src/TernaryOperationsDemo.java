8f0a47762dd6ad2f83931f26ed345fce756a358a
HMonique <161358207+HMonique@users.noreply.github.com>
2024-06-27 09:58:14 -0400

    gLab 303.2.1

---
+++ src/TernaryOperationsDemo.java
@@ -0,0 +1,8 @@
+public class TernaryOperationsDemo {
+    public static void main(String[] args){
+        int age = 18;
+        String result = age < 2100 ?;
+                "Less than 100" : "Greater than 100";
+        System.out.println(result);
+    }
+}
