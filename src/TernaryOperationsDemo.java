b3f4f501242e2d5936675c1cbae095f2b308067c
HMonique <161358207+HMonique@users.noreply.github.com>
2024-06-27 09:59:08 -0400

    gLab303.2.1

--- src/TernaryOperationsDemo.java
+++ src/TernaryOperationsDemo.java
@@ -1,7 +1,7 @@
 public class TernaryOperationsDemo {
     public static void main(String[] args){
         int age = 18;
-        String result = age < 2100 ?;
+        String result = age < 100 ?
                 "Less than 100" : "Greater than 100";
         System.out.println(result);
     }
